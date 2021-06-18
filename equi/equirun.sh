720a074bca705bd73ac127c2a42194d8f2852e90
Dario Shehni <dario.shehni@gmail.com>
2021-06-18 10:18:25 +0200

    equi caps

--- equi/equirun.sh
+++ equi/equirun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 03:00:00
-#SBATCH -p gpu
+#SBATCH -t 01:00:00
+#SBATCH -p gpu_titanrtx
 #SBATCH --gres=gpu:1
 
 #Run program 
 
-python deepcaps_hr.py
+python cifar.py
